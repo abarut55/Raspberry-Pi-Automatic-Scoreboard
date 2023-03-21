000fb0895d1221f9819807609eaa773cc6ff0f3d
abarut55 <112417990+abarut55@users.noreply.github.com>
2023-03-21 19:55:13 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,54 @@
+app = Flask(__name__)
+
+GPIO.setmode(GPIO.BCM)
+
+# set GPIO Pins
+GPIO_TRIGGER = 18
+GPIO_ECHO = 24
+
+# set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+score = 0
+
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    StartTime = time.time()
+    StopTime = time.time()
+
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+
+    return distance
+
+@app.route("/", methods=["GET"])
+def home():
+    global score
+    dist = distance()
+    if dist <= 1000:
+        score += 1
+    return render_template("index.html", score=score)
+
+if __name__ == '__main__':
+    try:
+        app.run(debug=True, host='0.0.0.0')
+    except KeyboardInterrupt:
+        GPIO.cleanup()
